690b1db1adcf804ef2c184f772c42ee51fe83d45
Gawsty Seri <sgtghostbo@gmail.com>
2020-07-13 21:12:24 +0100

    Updates

---
+++ precache-manifest.26c9fdbe2e38f767e67d0878e1dab9d3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76b98a67239064802ab95a078b4a48d6",
+    "url": "/splashtest/index.html"
+  },
+  {
+    "revision": "f3c24256866f45788dc0",
+    "url": "/splashtest/static/css/main.0a523d5a.chunk.css"
+  },
+  {
+    "revision": "71fc6f045c90ca04af9d",
+    "url": "/splashtest/static/js/2.fb65db24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/splashtest/static/js/2.fb65db24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3c24256866f45788dc0",
+    "url": "/splashtest/static/js/main.cdfdeaff.chunk.js"
+  },
+  {
+    "revision": "8ce6f760d9a282350d0a",
+    "url": "/splashtest/static/js/runtime-main.bab052c5.js"
+  },
+  {
+    "revision": "93ca6d44b79daf46b38e92880be4ec98",
+    "url": "/splashtest/static/media/laptop-1.93ca6d44.svg"
+  },
+  {
+    "revision": "6b7b3871f9d16b374d5fe21df98e24f4",
+    "url": "/splashtest/static/media/man-holding-box.6b7b3871.svg"
+  },
+  {
+    "revision": "c8abdd52dc1aebd8204352e2cadba673",
+    "url": "/splashtest/static/media/progress-bar.c8abdd52.svg"
+  },
+  {
+    "revision": "ac38cb47aecc9a773b298361e072f294",
+    "url": "/splashtest/static/media/woman-scooter.ac38cb47.svg"
+  }
+]);
